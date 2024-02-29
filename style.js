b1861a6c44f6827ff4ea2d4acca8d835244adafe
sameer5l <info.sameer25@gmail.com>
2024-02-29 10:47:48 +0500

    Initial commit

---
+++ style.js
@@ -0,0 +1,92 @@
+// //  Question 1
+// alert("Assalam w laikom")
+
+// // Question 2
+// var pss ="please! enter a valid password"
+// alert(pss)
+
+
+// // Question 3
+// var welcome =" Welcome to JS land... \n Happy Coading! " ;
+// alert(welcome)
+
+
+// Question 4
+
+// var welcome =" Welcome to JS land.. " ;
+// alert(welcome)
+
+// var welcoe =" Happy coading! \n Prevent this page for creating additional dilogs! " ;
+// alert(welcoe)
+
+// Question 5
+
+// console.log(" Hello.! i can run JS through my web browser's console ") ;
+// alert("Hello.! i can run JS through my web browser's console")
+
+// // Question 6
+// alert("Welcome to our HTML page")
+
+
+// Question 7
+
+
+
+
+
+
+// VARIABLES FOR STRINGS
+
+
+// // Question 1
+// var username="Sameer Shahzad"
+// alert(username)
+
+
+// Question 2
+// var myname="Sameer Shahzad"
+// alert(myname)
+
+
+// Question 3
+// var messeage="HEllow world"
+// alert(message)
+
+
+// Question 4
+// var name="Sameer Shahzad"
+// alert(name)
+// var age="17 year old"
+// alert(age)
+// var certified="certified mobile application development"
+// alert(certified)
+
+// Question 5
+// var pizza="Pizza\nPizz\nPiz\nPi\nP"
+// alert(pizza)
+
+// // Question 6
+// var email="My email address is info.sameer25@gmail.com"
+// alert(email)
+
+// // Question 7
+// var smat="A smarter way to learn\n JavaScript."
+// alert(smat)
+
+// Question 8
+// document.write("Yeah! I can right HTML content through java script")
+
+
+
+
+// // Question 9
+// var sm="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”."
+// alert(sm)
+
+
+// VARIABLE FOR NUMBERS
+
+// Question 1
+// var agee="I am 17 year old"
+// alert(agee)
+
